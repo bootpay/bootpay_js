ae7d7c09a15b8a1fc1c6a94fa748835ac33ffab9
Gosomi <gosomi@forelf.com>
2018-12-07 23:22:14 +0900

    2.0.13 버전 publish 준비

--- test/payment/payment.js
+++ test/payment/payment.js
@@ -1,142 +1,142 @@
 document.addEventListener('DOMContentLoaded', function () {
-    BootPay.setLogLevel(4);
-    BootPay.setMode('development');
-    BootPay.startTrace({
-        items: [
-            {
-                item_name: '아이템1',
-                item_img: 'https://www.bootpay.co.kr/sdklflksjdflkj',
-                unique: '1',
-                cat1: '카1',
-                cat2: '카2',
-                cat3: '카3'
-            },
-            {
-                item_name: '아이템2',
-                item_img: 'https://www.bootpay.co.kr/sdklflksjdflkj',
-                unique: '2',
-                cat1: '카1',
-                cat2: '카2',
-                cat3: '카3'
-            }
-        ]
-    });
-    document.getElementsByName('pg')[0].value = 'tpay';
-    document.getElementsByName('method')[0].value = 'card';
+	BootPay.setLogLevel(4);
+	BootPay.setMode('development');
+	BootPay.startTrace({
+		items: [
+			{
+				item_name: '아이템1',
+				item_img: 'https://www.bootpay.co.kr/sdklflksjdflkj',
+				unique: '1',
+				cat1: '카1',
+				cat2: '카2',
+				cat3: '카3'
+			},
+			{
+				item_name: '아이템2',
+				item_img: 'https://www.bootpay.co.kr/sdklflksjdflkj',
+				unique: '2',
+				cat1: '카1',
+				cat2: '카2',
+				cat3: '카3',
+			}
+		]
+	});
+	document.getElementsByName('pg')[0].value = 'kcp';
+	document.getElementsByName('method')[0].value = 'bank';
 });
 
 function doPayment() {
-    BootPay.request({
-        price: document.getElementsByName('price')[0].value,
-	    tax_free: document.getElementsByName('tax_free')[0].value,
-        application_id: '59a568d3e13f3336c21bf707',
-        name: '테스트 아이템',
-        phone: '01000000000',
-        order_id: (new Date()).getTime(),
-        pg: document.getElementsByName('pg')[0].value,
-        method: document.getElementsByName('method')[0].value,
-        show_agree_window: 0,
-        items: [
-            {
-                item_name: '테스트 아이템',
-                qty: 1,
-                unique: '123',
-                price: 1000
-            }
-        ],
-        user_info: {
-            email: 'test.bootpay.co.kr@gmail.com'
-        },
-        extra: {
-            expire_month: '36',
-	        vbank_result: 1,
-            quota: '0,2,3,4,5,6,7,8,9,10,11',
-            phone_number: '01000000000',
-            phone_fixed: 0,
-            phone_carrier: 'SKT'
-
-        }
-    }).error(function (data) {
-        var msg = "결제 에러입니다.: " + JSON.stringify(data)
-        alert(msg);
-        console.log(data);
-    }).cancel(function (data) {
-	    var msg = "결제 취소입니다.: " + JSON.stringify(data)
-        alert(msg);
-        console.log(data);
-    }).confirm(function (data) {
-        if (confirm('결제를 정말 승인할까요?')) {
-            console.log("do confirm data: " + JSON.stringify(data));
-            this.transactionConfirm(data);
-        } else {
-            var msg = "결제가 승인거절되었습니다.: " + JSON.stringify(data)
-            alert(msg);
-            console.log(data);
-        }
-    }).done(function (data) {
-        alert("결제가 완료되었습니다.");
-        console.log(data);
-    }).ready(function(data) {
-        console.log(data);
-    }).close(function() {
-        alert('닫힘!!');
-    });
+	BootPay.request({
+		price: document.getElementsByName('price')[0].value,
+		tax_free: document.getElementsByName('tax_free')[0].value,
+		application_id: '59a568d3e13f3336c21bf707',
+		name: '테스트\"s 아이템',
+		phone: '01000000000',
+		order_id: (new Date()).getTime(),
+		pg: document.getElementsByName('pg')[0].value,
+		method: document.getElementsByName('method')[0].value,
+		show_agree_window: 0,
+		account_expire_at: '2018-11-21 01:01:01',
+		items: [
+			{
+				item_name: '테스트 아이템',
+				qty: 1,
+				unique: '123',
+				price: 1000
+			}
+		],
+		user_info: {
+			email: 'test.bootpay.co.kr@gmail.com'
+		},
+		extra: {
+			expire_month: '36',
+			vbank_result: 1,
+			quota: '0,2,3,4,5,6,7,8,9,10,11',
+			phone_number: '01000000000',
+			phone_fixed: 0,
+			phone_carrier: 'SKT'
+		}
+	}).error(function (data) {
+		var msg = "결제 에러입니다.: " + JSON.stringify(data)
+		alert(msg);
+		console.log(data);
+	}).cancel(function (data) {
+		var msg = "결제 취소입니다.: " + JSON.stringify(data)
+		alert(msg);
+		console.log(data);
+	}).confirm(function (data) {
+		if (confirm('결제를 정말 승인할까요?')) {
+			console.log("do confirm data: " + JSON.stringify(data));
+			this.transactionConfirm(data);
+		} else {
+			var msg = "결제가 승인거절되었습니다.: " + JSON.stringify(data)
+			alert(msg);
+			console.log(data);
+		}
+	}).done(function (data) {
+		alert("결제가 완료되었습니다.");
+		console.log(data);
+	}).ready(function (data) {
+		console.log(data);
+	}).close(function () {
+		alert('닫힘!!');
+	});
 }
 
 function doAllPayment() {
-    BootPay.request({
-        price: document.getElementsByName('price')[0].value,
-	    tax_free: document.getElementsByName('tax_free')[0].value,
-        application_id: '59a568d3e13f3336c21bf707',
-        name: '테스트 아이템',
-        phone: '01000000000',
-        order_id: (new Date()).getTime(),
-        show_agree_window: 1,
-        items: [
-            {
-                item_name: '테스트 아이템',
-                qty: 1,
-                unique: '123',
-                price: 1000
-            }
-        ],
-        user_info: {
-            email: 'test.bootpay.co.kr@gmail.com'
-        },
-	    extra: {
-		    expire_month: '36'
-	    }
-    }).error(function (data) {
-        var msg = "결제 에러입니다.: " + JSON.stringify(data)
-        alert(msg);
-        console.log(data);
-    }).cancel(function (data) {
-        var msg = "결제 취소입니다.: " + JSON.stringify(data)
-        alert(msg);
-        console.log(data);
-    }).confirm(function (data) {
-        if (confirm('결제를 정말 승인할까요?')) {
-            console.log("do confirm data: " + JSON.stringify(data));
-            this.transactionConfirm(data);
-        } else {
-            var msg = "결제가 승인거절되었습니다.: " + JSON.stringify(data);
-            alert(msg);
-            console.log(data);
-        }
-    }).done(function (data) {
-        alert("결제가 완료되었습니다.");
-        console.log(data);
-    }).ready(function(data) {
-	    console.log(data);
-    });
+	BootPay.request({
+		price: document.getElementsByName('price')[0].value,
+		tax_free: document.getElementsByName('tax_free')[0].value,
+		application_id: '59a568d3e13f3336c21bf707',
+		name: "테스트's 아이템",
+		phone: '01000000000',
+		order_id: (new Date()).getTime(),
+		show_agree_window: 1,
+		items: [
+			{
+				item_name: '테스트 아이템',
+				qty: 1,
+				unique: '123',
+				price: 1000
+			}
+		],
+		user_info: {
+			email: 'test.bootpay.co.kr@gmail.com'
+		},
+		extra: {
+			expire_month: '36'
+		}
+	}).error(function (data) {
+		var msg = "결제 에러입니다.: " + JSON.stringify(data)
+		alert(msg);
+		console.log(data);
+	}).cancel(function (data) {
+		var msg = "결제 취소입니다.: " + JSON.stringify(data)
+		alert(msg);
+		console.log(data);
+	}).confirm(function (data) {
+		if (confirm('결제를 정말 승인할까요?')) {
+			console.log("do confirm data: " + JSON.stringify(data));
+			this.transactionConfirm(data);
+		} else {
+			var msg = "결제가 승인거절되었습니다.: " + JSON.stringify(data);
+			alert(msg);
+			console.log(data);
+		}
+	}).done(function (data) {
+		alert("결제가 완료되었습니다.");
+		console.log(data);
+	}).ready(function (data) {
+		console.log(data);
+	});
 }
 
 function changeMethod(e) {
-    switch (e.value) {
-        case 'card_rebill':
-            document.getElementsByName('price')[0].value = 0;
-            break;
-        default:
-            document.getElementsByName('price')[0].value = 3000;
-    }
+	switch (e.value) {
+		case 'card_rebill':
+			document.getElementsByName('price')[0].value = 0;
+			break;
+		default:
+			document.getElementsByName('price')[0].value = 3000;
+	}
 }
